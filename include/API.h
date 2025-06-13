6c07d18ccbfad1eebf4c6a47050d0d71728ce095
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2025-06-13 03:34:55 +0200

    Update API.h

--- include/API.h
+++ include/API.h
@@ -33,7 +33,7 @@ namespace SkyPromptAPI {
     constexpr ButtonID kMouseMove = 283;
     constexpr ButtonID kThumbstickMove = 284;
 
-    enum PromptType {
+    enum PromptType : std::uint8_t {
         kSinglePress,
         kHold,
         kHoldAndKeep,
@@ -49,7 +49,7 @@ namespace SkyPromptAPI {
 		RE::FormID refid;
 	};
 
-	enum PromptEventType {
+	enum PromptEventType : std::uint8_t {
 		kAccepted,
 		kDeclined,
         kRemovedByMod,
