562c114808e2a38dca04ad0b0661c90334e44924
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2025-12-10 09:42:42 +0100

    Add SendPromptForControl function to SkyPrompt
    Introduces SendPromptForControl, allowing prompts to be sent based on control bindings for keyboard, mouse, and gamepad. Registers the new function with the virtual machine for use in the SkyPrompt API.

--- include/PapyrusAPI/Bindings.cpp
+++ include/PapyrusAPI/Bindings.cpp
@@ -29,6 +29,34 @@ namespace {
         return false;
     }
 
+    bool SendPromptForControl(RE::StaticFunctionTag*, SkyPromptAPI::ClientID clientID, std::string text,
+                               SkyPromptAPI::EventID eventID, SkyPromptAPI::ActionID actionID,
+                               SkyPromptAPI::PromptType type, RE::TESForm* refForm,
+                               std::string a_controlName, int a_contextID, float progress) {
+        constexpr std::array devices = {RE::INPUT_DEVICE::kKeyboard, RE::INPUT_DEVICE::kMouse,
+                                        RE::INPUT_DEVICE::kGamepad};
+
+        std::vector<std::pair<RE::INPUT_DEVICE, SkyPromptAPI::ButtonID>> bindings;
+        for (size_t i = 0; i < devices.size(); ++i) {
+            auto a_device = devices[i];
+            auto a_key = RE::ControlMap::GetSingleton()->GetMappedKey(a_controlName, a_device,
+                                                                      static_cast<RE::ControlMap::InputContextID>(
+                                                                          a_contextID));
+            bindings.emplace_back(a_device, a_key);
+        }
+
+        if (PapyrusAPI::AddPrompt(clientID, text, eventID, actionID, type, refForm, bindings, progress)) {
+            std::shared_lock lock(PapyrusAPI::mutex_);
+            if (const auto it = PapyrusAPI::papyrusSinks.find(clientID); it != PapyrusAPI::papyrusSinks.end()) {
+                auto& sinks = it->second;
+                if (const auto a_sink = sinks.find({eventID, actionID}); a_sink != sinks.end()) {
+                    return SkyPromptAPI::SendPrompt(a_sink->second.get(), clientID);
+                }
+            }
+        }
+        return false;
+    }
+
     void RemovePrompt(RE::StaticFunctionTag*, SkyPromptAPI::ClientID clientID, SkyPromptAPI::EventID eventID,
                       SkyPromptAPI::ActionID actionID) {
         std::shared_lock lock(PapyrusAPI::mutex_);
@@ -114,6 +142,7 @@ bool PapyrusAPI::Register(RE::BSScript::IVirtualMachine* vm) {
     vm->RegisterFunction("RegisterForSkyPromptEvent", "SkyPrompt", RegisterForSkyPromptEvent);
     vm->RegisterFunction("UnregisterFromSkyPromptEvent", "SkyPrompt", UnregisterFromSkyPromptEvent);
     vm->RegisterFunction("SendPrompt", "SkyPrompt", SendPrompt);
+    vm->RegisterFunction("SendPromptForControl", "SkyPrompt", SendPromptForControl);
     vm->RegisterFunction("RemovePrompt", "SkyPrompt", RemovePrompt);
     vm->RegisterFunction("RequestTheme", "SkyPrompt", RequestTheme);
 
