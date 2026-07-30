a688be7d3418f2f5c7d86f550c8a0a334ac0dc56
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2026-07-30 03:30:42 +0200

    Bump default client API major version to 2
    Updates `ProcessRequestClientID` in `Service.h` to default to API version 2.0 instead of 1.0, so callers that rely on default arguments now request the current major protocol by default.

--- include/Service.h
+++ include/Service.h
@@ -11,7 +11,7 @@ static_assert(sizeof(SkyPromptAPI::ActionID) == sizeof(uint16_t), "ActionID size
 
 extern "C" DLLEXPORT bool ProcessSendPrompt(const SkyPromptAPI::PromptSink* a_sink, SkyPromptAPI::ClientID a_clientID);
 extern "C" DLLEXPORT void ProcessRemovePrompt(const SkyPromptAPI::PromptSink* a_sink, SkyPromptAPI::ClientID a_clientID);
-extern "C" DLLEXPORT SkyPromptAPI::ClientID ProcessRequestClientID(int a_major = 1, int a_minor = 0);
+extern "C" DLLEXPORT SkyPromptAPI::ClientID ProcessRequestClientID(int a_major = 2, int a_minor = 0);
 extern "C" DLLEXPORT bool ProcessRequestHandshake(SkyPromptAPI::ClientID a_clientID, Handshake::HandshakeKey a_key, Handshake::HandshakeKey a_otherKey);
 extern "C" DLLEXPORT bool ProcessRequestTheme(SkyPromptAPI::ClientID a_clientID, std::string_view theme_name);
 
