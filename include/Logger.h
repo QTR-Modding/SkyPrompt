afd94fe29237e86280f7e4f6996b259f219948f0
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2026-09-01 05:02:30 +0200

    Set log pattern and keep custom logger
    Adds an explicit spdlog pattern to include timestamp, thread id, level, and message for clearer logs. Also updates `SKSE::Init` to pass `false` so SKSE does not override the plugin's logger setup.

--- include/Logger.h
+++ include/Logger.h
@@ -8,6 +8,7 @@ static void SetupLog() {
     auto logFilePath = *logsFolder / std::format("{}.log", pluginName);
     auto fileLoggerPtr = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logFilePath.string(), true);
     auto loggerPtr = std::make_shared<spdlog::logger>("log", std::move(fileLoggerPtr));
+    loggerPtr->set_pattern("[%H:%M:%S.%e] [%t] [%l] %v");
     spdlog::set_default_logger(std::move(loggerPtr));
     #ifndef NDEBUG
     spdlog::set_level(spdlog::level::trace);
