f010346f37286d7eaadeb5d4257a5a9f81497119
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2026-09-08 12:36:37 +0200

    Use VR offsets for rendering and input hooks

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -118,7 +118,7 @@ void ImGui::Renderer::Install() {
                                                            CreateD3DAndSwapChain::thunk);
 
     const REL::Relocation<std::uintptr_t> target2{REL::RelocationID(75461, 77246)}; // BSGraphics::Renderer::End
-    DrawHook::func = trampoline.write_call<5>(target2.address() + 0x9, DrawHook::thunk);
+    DrawHook::func = trampoline.write_call<5>(target2.address() + REL::Relocate(0x9, 0x9, 0x15), DrawHook::thunk);
 
     MenuHook<RE::LoadingMenu>::InstallHook(RE::VTABLE_LoadingMenu[0]);
 }
@@ -244,7 +244,7 @@ bool InputHook::ProcessInput(RE::InputEvent* event) {
 void ImGui::Renderer::InstallInputHook() {
     auto& trampoline = SKSE::GetTrampoline();
     const REL::Relocation<std::uintptr_t> target3{REL::RelocationID(67315, 68617)};
-    InputHook::func = trampoline.write_call<5>(target3.address() + 0x7B, InputHook::thunk);
+    InputHook::func = trampoline.write_call<5>(target3.address() + REL::Relocate(0x7B, 0x7B, 0x81), InputHook::thunk);
 }
 
 template <typename MenuType>
